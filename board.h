04885b62725f378ce5098fb7769c224df1423ef6
mruzo <mruzmaykin@gmail.com>
2018-11-19 12:10:26 -0800

    added header

---
+++ board.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+class Board
+{
+    public:
+        Board();
+        ~Node();
+        
+    private:
+	char board[8][8];
+};
+
+Board::Board()
+Board::~Board()
+
+#endif // NODE_H
